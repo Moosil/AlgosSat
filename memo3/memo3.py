7903840de91ca9eac5a953bf1ac57861199102e9
Moosil <rowancritchett@outlook.com>
2026-09-25 16:14:46 +1000

    fix some old code, add some missing elements, update mini-facility viewer

--- memo3/memo3.py
+++ memo3/memo3.py
@@ -657,7 +657,7 @@ def graph_drawer_impl(itertools, mcolors, nx, plt, random, seed_input):
 def _(mo, np, re):
     class PseudocodeExplorer:
         def __init__(self, fp: str):
-            self.raw_pseudocode = mo.watch.file(fp).read_text()
+            self.raw_pseudocode = open(fp, encoding="utf-8").read()
             self.full_pseudocode = self._parse_pseudocode(self.raw_pseudocode)
 
         def get_fn_fancy(self, name: str, font_size: int = 12, numbered: bool = False, *, start_offset: int = 0, start_offset_function_name_prefix: bool = True, end_offset: int = 0, start_elipsis=True, end_elipsis=True):
@@ -747,8 +747,10 @@ def _(mo, np, re):
             for command in ["AND", "OR", "NOT", "RAISE", "DO", "THEN", "IN", "TO", "RETURN", "BREAK", "TRUE", "FALSE"]:
                 res = re.sub(fr"(?:(?<=\s)|(?<=&#9;)|(?<=\<br\>)){command}(?:(?=\s)|(?=&#9;)|(?=\<br\>))", f"<span class='pseudocode-command'>{command}</span>", res)
 
-            for operator in [r"<-", "=", ">", "<", "<=", ">=", "+", "-"]:
-                res = re.sub(fr"(?<= ){operator}(?= )", f"<span class='pseudocode-op'>{operator}</span>", res)
+            for operator in ["<-", "=", ">", "<", "<=", ">=", r"+", "-"]:
+                res = re.sub(fr"(?<= )\{operator}(?= )", f"<span class='pseudocode-op'>{operator}</span>", res)
+
+            res = re.sub(fr"(?<= )-(?=[0-9])", f"<span class='pseudocode-op'>-</span>", res)
 
             for item in ["∞"]:
                 res = res.replace(str(item), f"<span class='pseudocode-bracket'>{item}</span>")
@@ -799,6 +801,7 @@ def _(mo, np, re):
             res = find_all(res, "PROCEDURE</span>", syntax_highlight_proc)
             res = find_all(res, "FUNCTION</span>", syntax_highlight_proc)
             res = res.replace("&#9;", "  ")
+            res = re.sub(r"\,", r"<span class='pseudocode-bracket'>,</span>", res)
 
             return res
 
@@ -860,7 +863,7 @@ def _(mo):
     # 2 Problem Abstraction
     Let $G = (V_w, E_w, w)$ be a meta-graph, with $V_w=\{W_1, W_2, \dots, W_k\}$ being a set of undirected weighted graphs, $E_w \subseteq \{\{u, v\} \vert u \in V_n, v \in V_m, n \neq m\}$ being a set of edges between adjacent wings, $W_n, W_m$ of the facility, with $k$ being the number of wings in the facility, and $\forall n \leq k, W_n = (V_n, E_n)$.
 
-    $V = V_1 \cup V_2 \cup \dots \cup V_k$ and $\forall n, m \leq k, V_n \cap V_m = \varnothing \iff n \neq m$ and $V_n = V_m \iff n = m$, with $V$ representing the salient sectors of the facility $E = E_1 \cup E_2 \cup \dots \cup E_k$ and $\forall n, m \leq k, E_n \cap E_m = \varnothing \iff n \neq m$ and $E_n = E_m \iff n = m$, with $E$ representing the paths between those adjacent salient sectors, and positive integer edge weight function $w: E \cup E_w \to \mathbb{N}$ representing the total cost of traversing the span of sectors  which are adjacent to just two other sectors and between two salient sectors. If $(u, v) \notin E$, define $w(u, v) = \infty$ and if $u = v$, defined $w(u, v) = 0$.
+    $V = V_1 \cup V_2 \cup \dots \cup V_k$ and $\forall n, m \leq k, V_n \cap V_m = \varnothing \iff n \neq m$ and $V_n = V_m \iff n = m$, with $V$ representing the salient sectors of the facility $E = E_1 \cup E_2 \cup \dots \cup E_k$ and $\forall n, m \leq k, E_n \cap E_m = \varnothing \iff n \neq m$ and $E_n = E_m \iff n = m$, with $E$ representing the paths between those adjacent salient sectors, and positive integer edge weight function $w: E \cup E_w \to \mathbb{Z}^+$ representing the total cost of traversing the span of sectors  which are adjacent to just two other sectors and between two salient sectors. If $(u, v) \notin E$, define $w(u, v) = \infty$ and if $u = v$, defined $w(u, v) = 0$.
 
     We will designate source vertex $s \in V$, the set of sink vertices $X \subseteq V$, and the set of supply vertices $S \subseteq V$, each representing the entry, exit, and supply unit-containing sectors respectively.
 
@@ -878,28 +881,17 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ## 2.1 Main Problem Instance
-    We have been tasked with creating an algorithm that works for a facility with the following values: $|V_w| \in [2, 4]$, $|E_w| = 2(|V_w| - 1)$, $|S| = 50$, $C = 5$, $\delta: S \to [1, 3]$, $p: S \to \mathbb{{N}}$. For other problem instances, the algorithm may solve them, but that has been a side-effort, and incorrectness can be found with lower budgets, supply capacities and supply counts in the facility.
+    ## 2.1 Signature Specification
+    $\text{ember\_rescue}: \text{Graph} \times \text{Vertex} \times \text{Set}[\text{Vertex}] \times \text{Set}[\text{Vertex}] \times \text{Map}[\text{Vertex}, \mathbb{N}] \times \text{Map}[\text{Vertex}, \mathbb{N}] \times \text{List}[\text{SupplyID}] \times \text{Map}[\text{Vertex}, \text{SupplyID}] \times \text{Set}[\text{SupplyID}] \times \mathbb{N} \to \text{List}[\text{Tuple}[\text{Vertex}, \mathbb{Z}^+, \mathbb{Z}^+, \mathbb{Z}^+]]$
     """
           )
     return
 
 
 @app.cell(hide_code=True)
-def _(mo):
-    mo.md(
-        r"""
-            ## 2.2 Signature Specification
-            $\text{ember\_rescue}: \text{Graph} \times \text{Vertex} \times \text{Set}[\text{Vertex}] \times \text{Set}[\text{Vertex}] \times \text{Map}[\text{Vertex}, \mathbb{N}] \times \text{Map}[\text{Vertex}, \mathbb{N}] \times \text{List}[\text{SupplyID}] \times \text{Map}[\text{Vertex}, \text{SupplyID}] \times \text{Set}[\text{SupplyID}] \times \mathbb{N} \to \text{List}[\text{Tuple}[\text{Vertex}, \mathbb{Z}^+, \mathbb{Z}^+, \mathbb{Z}^+]]$
-            """
-        )
-    return
-
-
-@app.cell(hide_code=True)
 def output_constraints(mo):
     mo.md(r"""
-    ## 2.3 Output Constraints
+    ## 2.2 Output Constraints
     To concisely present a set of constraints on the output, it is necessary to define the following functions:
 
     $\delta_{sum}(W, n) = \displaystyle\sum_{\mathclap{k=1}}^n \begin{cases}
@@ -933,17 +925,44 @@ def output_constraints(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
+    mo.md(
+        r"""
+            ## 2.3 Assumptions
+            To create a coherant algorithm, assumptions must be made to restrict the input in ways that allow more efficient approaches to be taken. The following assumptions have held against all facilty blueprints, historical records and facilties seen in testing, and allow large optimisations of the algorithm.
+            - The facility's wings do not contain intra-wing cycles (essential for depth-first search)
+            - The facility is fully connected
+            - The facility is finite, and therefore there is a finite number of supplies in the facility
+            - Each supply has a positive integer weight and priority (essential for knapsack problem dynamic program procedure)
+            - There is enough energy to get to an exit.
+            """
+        )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+            ## 2.4 Main Problem Instance
+            We have been tasked with creating an algorithm that works for a facility with the following values: $|V_w| \in [2, 4]$, $|E_w| = 2(|V_w| - 1)$, $|S| = 50$, $C = 5$, $\delta: S \to [1, 3]$, $p: S \to \mathbb{{N}}$. For other problem instances, the algorithm may solve them, but that has been a side-effort, and incorrectness can be found with lower budgets, supply capacities and supply counts in the facility.
+            """
+        )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
     _title = mo.md(fr"""
-    ## 2.4 ADT Revisions
-    ### 2.4.1 Justification
+    ## 2.5 ADT Revisions
+    ### 2.5.1 Justification
     - Making more functions inline, which reduces unnecessary copy instructions
     - adding indexed pop/push functions
     - adding set insertion/removal functions.
 
-    ### 2.4.2 Semantic Changes
+    ### 2.5.2 Semantic Changes
     - Algorithm and procedure parameters are now passed by reference instead of copies.
     <hr>
-    ### 2.4.3 New Signatures
+    ### 2.5.3 New Signatures
     """)
 
     _graphs_md = mo.md(r"""
@@ -1035,7 +1054,7 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ## 2.5 Data Model Revisions
+    ## 2.6 Data Model Revisions
     Due to the dropping supplies feature of the revised problem, abstracting 2-degree vertices as weight between 3+ degree vertices loses data. This is because supplies must be dropped on these 2-degree sectors in optimal solutions.
 
     This change causes a redesign, as $|V|$ and $|E|$ are now larger.
@@ -1046,7 +1065,7 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ## 2.6 Revised Algorithm
+    ## 2.7 Revised Algorithm
     the TL;DR of this algorithm is "DFS on $G$, clearing between each non-3-degree vertex recursively, bring back supplies when total weight on backtrack $\geq 5$".
 
     The algorithm first, like the previous algorithm, determines which supplies have already been collected using $F$ and $M$, removing them from $S$. It will then flatten $G$ to a single graph and run Dijkstra's algorithm from $s$. It will find the shortest path to an exit vertex in $X$ and save that cost in a variable. After which it will run a knapsack problem on the supplies' weights and priorities with a budget equal to $B$ minus that exit run distance, with tuned supply costs based on empirical data. It will then, for each supply get which junctions it should go through to reach them.
@@ -1069,15 +1088,19 @@ def _(mo):
     mo.md(r"""
     # 3 Algorithm Quality
     ## 3.1 Efficiency
-    The revised algorithm is much more efficient that memo 2's algorithm, running in $O(n^k), k \in N$ for $n$ being the worst-case variable and $k$ being independent of $n$, compared with memo 2's $O(n^2 2^n)$ complexity. This latter complexity is intractable and would not work on the larger supply count in the updated situation.
+    The revised algorithm is much more efficient that memo 2's algorithm, running in $O(n^6)$, compared with memo 2's $O(n^2 2^n)$ complexity, where $n$ is the byte-length of the input. This latter complexity is intractable and would not work on the larger supply count in the updated situation, as it grows faster than polynomial time. The former complexity is tractable, being polynomial with degree 6.
 
-    The algorithm proposed in this memo could be more efficient, and the reasons for not implementing these will be discussed in Section 5.
+    When applied to real-world scenarios, even though a version of memo 2's algorithm adapted to the new problem would give an optimal supply collection, it would likely take years or may never complete in the time left in the universe on a super-computer, let alone a drone. The space complete $O(n^3 2^n)$ would also become a problem, with it being unrunnable after 35 supplies even if that cost has coefficient 1 (which is has at least).
+
+    Alternatively, the solution proposed in this memo runs in $O(n^6)$ time and $O(n^3)$ space, making it useable on larger facilities without changes. This makes it useful on lower-end mobile CPUs like those embedded in lower-end drones.
 
     ## 3.2 Coherence
     Both problem abstractions were concise and coherant, without redundant elements. They also aim to be as unopinionated as is useful: allowing a variety of algorithms to be used without being so general as to become useless.
 
+    This memo's algorithm has an edge against the more general algorithm of the previous memo: memoised dp exhaustive search doesn't fit any specific problem, while the depth-first search utilised assumptions of the facility to reduce the complexity of the algorithm to being tractable.
+
     ## 3.3 Fitness for Purpose
-    Due to the heuristic nature of the algorithm, a memo 2-like exact algorithm will arrive at a better solution than this memo's algorithm. This of course comes at the cost of efficiency, and difficulty to encapsulate all features of the problem: multiple-trips, trip-dependent supply collection costs and dropping supplies, into an exact algorithm, which is why a heuristic algorithm is more fit for purpose than an exact algorithm like memo 2's
+    Due to the heuristic nature of memo 3's algorithm, a memo 2-like algorithm &mdash; which is exact and searches all possible best solutions &mdash; will arrive at a better solution than this memo's algorithm. This of course comes at the cost of efficiency, and difficulty to encapsulate all features of the problem: multiple-trips, trip-dependent supply collection costs and dropping supplies, into an exact algorithm, which is why a heuristic algorithm is more fit for purpose than an exact algorithm like memo 2's.
 
     ## 3.4 Counter-example
     """)
@@ -1193,22 +1216,33 @@ def _(GraphDrawer, plt, random):
 
             drone = path[-1][0], path[-1][1]
 
+        storage_i = 0
         for i, (x, y, w) in enumerate(supplies):
-            if i in storage or (x == 0 and y == 0):
+            if x == 0 and y == 0:
                 continue
-            ax.plot(
-                x + 0.5, y + 0.5, marker='*', markersize=40,
-                color=COL_SUPPLY, markeredgecolor=COL_ENTRY,
-                markeredgewidth=3, zorder=7
-            )
-            ax.text(x + .5, y + .5 - .01, w, fontsize=16, color="#ffffff", zorder=8, ha="center", va="center")
+
+            if i in storage:
+                ax.plot(
+                    drone[0] + 0.8 + storage_i * .25, drone[1] + 0.2, marker='*', markersize=20,
+                    color=COL_SUPPLY, markeredgecolor=COL_ENTRY,
+                    markeredgewidth=3, zorder=9
+                )
+                ax.text(drone[0] + 0.8 + storage_i * .25, drone[1] + 0.2 - .01, w, fontsize=16, color="#ffffff", zorder=10, ha="center", va="center")
+                storage_i += 1
+            else:
+                ax.plot(
+                    x + 0.5, y + 0.5, marker='*', markersize=40,
+                    color=COL_SUPPLY, markeredgecolor=COL_ENTRY,
+                    markeredgewidth=3, zorder=9
+                )
+                ax.text(x + .5, y + .5 - .01, w, fontsize=16, color="#ffffff", zorder=10, ha="center", va="center")
 
         ax.plot(
             drone[0] + 0.5, drone[1] + 0.5, marker="o", markersize=40,
             color=COL_DRONE, markeredgecolor="#94283c",
-            markeredgewidth=3, zorder=9
+            markeredgewidth=3, zorder=11
         )
-        ax.text(drone[0] + 0.515, drone[1] + 0.495, r"$\mathbf{C}_1$", fontsize=20, color="#ffffff", zorder=10, ha="center", va="center")
+        ax.text(drone[0] + 0.515, drone[1] + 0.495, r"$\mathbf{C}_1$", fontsize=20, color="#ffffff", zorder=12, ha="center", va="center")
 
         ax.set_aspect('equal')
         ax.axis('off')
@@ -1324,7 +1358,7 @@ def _(mo, pseudocode_explorer, re):
                       <li>he double nested while loop below (92) (101) has the outer running $C$ times at most for dropping at most $C$ 1-weight supplies (given supply weights $\in \mathbb{N}$), while the inner loop amortises to $|S|$ runnings for the same reason as the previous part.</li></ul></li></ul></div>"""
                 case "ember_rescue":
                     return r"""The $|X| |V|$ cost can be attributed to finding length of paths to each exit (12 - 19). The $|V|^3$ cost is from reconstructing the paths between at most $|V| |S|$ vertices (44, 54), in the worst case where CRUDY-1 collects each supply one at a time. The rest of the cost is from the `clear_branch` call (41) which is amortised on each vertex (hence it's not $|V| \times$ that cost).
-                
+        
     In terms of the byte-length of the input $n$, this gives $O(n^6)$ time complexity."""
 
         @classmethod
@@ -1339,7 +1373,7 @@ def _(mo, pseudocode_explorer, re):
                 "S": "set of supply vertices"
             }
 
-            big_os = mo.watch.file("memo3/big_os.txt").read_text()
+            big_os = open("memo3/big_os.txt", encoding="utf-8").read()
             t_n_big_o_latex = list(filter(lambda x: name in x, big_os.split('\n')))[0].replace(f"{name}: ", "")
 
             pseudocode = pseudocode_explorer.get_fn_fancy(name, numbered=True)
@@ -1626,7 +1660,7 @@ def _(mo, np, pd, plt):
         return fig
 
     _df = pd.read_csv("memo3/data/flame_facility_small.csv")
-    mo.lazy(_get_flame_graph(_df, sum(1 for _ in mo.watch.file("memo3/data/data_facility_small.csv").read_text())), show_loading_indicator=True)
+    mo.lazy(_get_flame_graph(_df, sum(1 for _ in open("memo3/data/data_facility_small.csv", encoding="utf-8").read())), show_loading_indicator=True)
     return
 
 
@@ -2123,7 +2157,7 @@ def _(get_fig, mo):
         rf"""
     <span style="color: var(--ctp-mocha-subtext0); ">Figure {get_fig("Budget vs Budget Used")}</span>
 
-    Figure {get_fig("Budget vs Budget Used")} shows the budget vs budget used, along with a line with gradient = 1. This shows the 4 outliers that go over budget: all between 2000 and 6000 budget. This likely means the cost function should take the budget amount as an input.
+    Figure {get_fig("Budget vs Budget Used")} shows the budget vs budget used, along with a line with gradient = 1. This shows the 4 outliers that go over budget: all between 2000 and 6000 budget. It also shows the gap between the budget and budget used widens as the budget gets larger. This likely means the cost function should take the budget amount as an input.
 
     This cost function optimisation could also be done by a neural network and the `get_weight_cost` function could become a neural network instead of a multi-variable mathematical function. In future, this network could also take as input the rest of the supplies' distances from the current or various other variables to more closely approximate the real cost which is dependent on all these factors
     """
@@ -2265,7 +2299,9 @@ def _(get_fig, mo):
 
     The algorithm has greater costs in clear_branch, likely due to me treating copying of lists into other lists to be constant time, which it isn't in practise. There are likely also other costs like function call, vector resizing and non-constant costs for each ADT operation which contribute to the predicted operation count being different from the one in the implementation.
 
-    For this algorithm to be suitable for real-world problems &mdash; where time and effort of computation directly reduces battery and can be the difference between a successful extraction and a failed one &mdash; this algorithm sacrifices optimality for its speed: 1.5 MOP/s can be calculated in 10ms on a 120MOP/s CPU. Due to the main DFS structure of `clear_branch` it is easily parallelisable with few changes, which would also aids its computational efficiency.
+    For this algorithm to be suitable for real-world problems &mdash; where time and effort of computation directly reduces battery and can be the difference between a successful extraction and a failed one &mdash; this algorithm sacrifices optimality for its speed: 1.5 MOP can be calculated in 10ms on a 120MOP/s CPU. Due to the main DFS structure of `clear_branch` it is easily parallelisable with few changes, which would also aids its computational efficiency.
+
+    With the previous dynamic programming approach, 50 supplies would not be completable both due to the time to complete and the storage to hold the memo-table. 5 supplies was around the limit for that algorithm, as it was close to going over the one second arbitrary budget I decided on. Each running of the algorithm on 50 supplies would take 1MOP and this would have to be done $\approx 10 \binom{{50}}{{5}} = 21,187,600$ times. This would be computable by computer networks and super computers but out of the range of embedded systems, and this doesn't even account for the added complexity of non-uniform supply weights, priorities and the budget not being enough to pick up all the supplies.
     """
         )
     return
